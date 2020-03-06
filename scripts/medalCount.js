dc33a77fe25205b14c846d7cfe9640d950a4ed03
bcranton <bcranton@users.noreply.github.com>
2020-03-06 11:06:34 -0700

    Fixed comment

--- scripts/medalCount.js
+++ scripts/medalCount.js
@@ -1,13 +1,4 @@
-// JavaScript source code
-
-/* automatic medal totaller
- * 
- * Assumes Totals table is set up but scores have not been populated.  Will work on Totals table that is populated too though.
- * Contains code to copy player names to the totals table but has been commented out.  Feel free to uncomment.
- * Points not tallied as there's no easy way to differentiate a score (27,200) and a different quantifier (2 Targets, for example)
- * If these were split out into different elements, or sub elements (<td>12,000</td><td>0<span class="comment">2 Targets</span></td>)
- * then it would be easier to tally and populate totals.
- * */
+// Automatically calculates the total medals on the bottom row on medals.html
 
 //init variables
 var firstCount = 0, secondCount = 0, thirdCount = 0;
@@ -30,5 +21,5 @@ for (var i = 1; i <= 3; i++) {
     $('table:nth-child(3) > tbody:nth-child(3) > tr > td:nth-child(' + i + ')').html(thirdCount + 'x <i class="fas fa-medal third"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + secondCount + 'x <i class="fas fa-medal second"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + firstCount + 'x <i class="fas fa-medal first"></i>');
 
     firstCount = 0, secondCount = 0, thirdCount = 0
-    
+
 }
