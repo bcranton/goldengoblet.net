7766d5f89bbd2a388c3a23d96538f42582527777
Joshua <joshua.noble@gm.com>
2020-03-13 15:29:44 -0400

    added logic to automatically add the medal icons based on the player's score in the Competitor.js file

--- scripts/medalCount.js
+++ scripts/medalCount.js
@@ -1,45 +1,37 @@
 // Automatically calculates the total medals on the bottom row on medals.html
 
 //init variables
-let danScore = 0, malfScore = 0, NLScore = 0;
 
-
-
-
-//loop through each column that contains scores.  First column contains episode # so we skip it.
-for (let i = 1; i <= 3; i++) {
-    let firstCount = 0, secondCount = 0, thirdCount = 0;
-    //go through each row and tally up the scores.
-    $('table:nth-child(3) > tbody:nth-child(3) > tr > td:nth-child(' + i + ') > i').each(function (a, b) {
-        if ($(this).hasClass('first')) {
-            firstCount++;
+for (c of allCompetitors) {
+    for (let p = 0; p < 3; p++) {
+        var img = document.createElement('span');
+        img.style.fontStyle = 'bold';
+        if (p == 0) {
+            img.innerHTML = `${c.bronze}x <i class ="fas fa-medal third"></i>`
+        }else if (p == 1) {
+            img.innerHTML = `${c.silver}x <i class ="fas fa-medal second" style="font-style:none"></i>`
+        }else {
+            img.innerHTML = `${c.gold}x <i class ="fas fa-medal first"></i>`
         }
-        else if ($(this).hasClass('second')) {
-            secondCount++;
-        }
-        else if ($(this).hasClass('third')) {
-            thirdCount++;
-        }
-
-    })
-    $('table:nth-child(3) > tbody:nth-child(4) > tr > td:nth-child(' + i + ')').html(thirdCount + 'x <i class="fas fa-medal third"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + secondCount + 'x <i class="fas fa-medal second"></i>&nbsp;&nbsp;&nbsp;&nbsp;' + firstCount + 'x <i class="fas fa-medal first"></i>');
-
-    if (i == 1) {
-        danScore = firstCount * 3 + secondCount * 2 + thirdCount;
+        document.querySelector(`table:nth-child(3) > tbody:nth-child(4) > tr > td:nth-child(${allCompetitors.indexOf(c)+1})`).appendChild(img).appendChild(document.createElement('span'));
+        document.querySelectorAll(`table:nth-child(3) > tbody:nth-child(4) > tr > td:nth-child(${allCompetitors.indexOf(c)+1}) > span > span`)[p].style = "padding: 1.5%"
     }
-
-    else if (i == 2) {
-        malfScore = firstCount * 3 + secondCount * 2 + thirdCount;
+    var body = ''
+    for (let b = 0; b < c.bronze-1; b++) {
+        body += '<i class="fas fa-medal third"></i>';
     }
-
-    else {
-        NLScore = firstCount * 3 + secondCount * 2 + thirdCount;
+    for (let s = 0; s < c.silver-1; s++) {
+        body += '<i class="fas fa-medal second"></i>';
     }
-
-
-    firstCount = 0, secondCount = 0, thirdCount = 0;
+    for (let g = 0; g < c.gold-1; g++) {
+        body += '<i class="fas fa-medal first"></i>';
+    }
+    document.querySelector(`#medals-bg > div > table > tbody:nth-child(3) > tr > td:nth-child(${allCompetitors.indexOf(c)+1})`).innerHTML = body;
+    
+    
 
 }
+
 
 
 for (c of allCompetitors) {
@@ -59,3 +51,4 @@ for (c of allCompetitors) {
     }
     $(`table:nth-child(3) > tbody:nth-child(4) > tr:nth-child(2) > td:nth-child(${allCompetitors.indexOf(c)+1})`).html(`<i class="fas fa-trophy ${place}"></i> ` + c.score + 'pts');
 }
+
