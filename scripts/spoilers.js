2dbec65f9da941828850c1a9d28d5cd330649d09
bcranton <bcranton@users.noreply.github.com>
2020-05-08 15:57:23 -0700

    fix?

--- scripts/spoilers.js
+++ scripts/spoilers.js
@@ -1,10 +1,4 @@
-$(document).ready(function () {
-    $('.unit-toggler').change(function () {
-        $('.results #spoilerSpan').toggle();
-        stateChange();
 
-    })
-});    
 $(document).ready(function () {
     var spoilerValue = window.localStorage.getItem("spoiler")
 
@@ -22,67 +16,72 @@ $(document).ready(function () {
         scoreCount();
 
     }
-});
+})
+$('.unit-toggler').change(function () {
+    $('.results #spoilerSpan').toggle();
+    stateChange();
+
+})
 
 function stateChange() {
-        if (spoilerValue == "show") {
-            spoilerValue = "hide";
-            window.localStorage.setItem("spoiler", spoilerValue);
-        }
-        else {
-            spoilerValue = "show";
-            window.localStorage.setItem("spoiler", spoilerValue);
-        }
-        scoreCount();
+    if (spoilerValue == "show") {
+        spoilerValue = "hide";
+        window.localStorage.setItem("spoiler", spoilerValue);
+    }
+    else {
+        spoilerValue = "show";
+        window.localStorage.setItem("spoiler", spoilerValue);
+    }
+    scoreCount();
 
-    };
+}
 
 
 function scoreCount() {
-        let firstCount = 0, secondCount = 0, thirdCount = 0, points = 0, pointsTemp = 0;
+    let firstCount = 0, secondCount = 0, thirdCount = 0, points = 0, pointsTemp = 0;
 
-        let playerNameAndLink = '';
+    let playerNameAndLink = '';
 
-        //loop through each column that contains scores.  First column contains episode # so we skip it.
-        for (let i = 2; i <= 4; i++) {
-            playerNameAndLink = $('table:nth-child(3) > thead > tr > th:nth-child(' + i + ') > a').clone();
-            //go through each row and tally up the scores.
-            $('table:nth-child(3) > tbody > tr > td:nth-child(' + i + ') > a > #spoilerSpan:nth-child(2)').each(function (a, b) {
-                if ($(this).hasClass('first')) {
-                    firstCount++;
-                }
-                else if ($(this).hasClass('second')) {
-                    secondCount++;
-                }
-                else if ($(this).hasClass('third')) {
-                    thirdCount++;
-                }
-                if (!$(this).hasClass("comment")) {
-                    // If the row is not part of the "comment" class
-                    // Assign pointsTemp to itself plus the value of the row, with any commas removed, and converted to a floating point
-                    pointsTemp += parseFloat(($(this).text().replace(",", "")));
-                    // Points is equal to pointsTemp, but uses this regex to add the commas back into the number
-                    points = pointsTemp.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                }
-                $('table:nth-child(4) > thead > tr > th:nth-child(' + i + ')').html(playerNameAndLink);
-                //insert the scores into the relevant medal tally
-                if (spoilerValue == "show") {
-                    $('table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(' + i + ')').text(thirdCount);
-                    $('table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(' + i + ')').text(secondCount);
-                    $('table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(' + i + ')').text(firstCount);
-                    $('table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(' + i + ')').text(points);
-                }
-                if (spoilerValue == "hide") {
-                    $('table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(' + i + ')').text("Spoilers");
-                    $('table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(' + i + ')').text("Spoilers");
-                    $('table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(' + i + ')').text("Spoilers");
-                    $('table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(' + i + ')').text("Spoilers");
-                }
+    //loop through each column that contains scores.  First column contains episode # so we skip it.
+    for (let i = 2; i <= 4; i++) {
+        playerNameAndLink = $('table:nth-child(3) > thead > tr > th:nth-child(' + i + ') > a').clone();
+        //go through each row and tally up the scores.
+        $('table:nth-child(3) > tbody > tr > td:nth-child(' + i + ') > a > #spoilerSpan:nth-child(2)').each(function (a, b) {
+            if ($(this).hasClass('first')) {
+                firstCount++;
+            }
+            else if ($(this).hasClass('second')) {
+                secondCount++;
+            }
+            else if ($(this).hasClass('third')) {
+                thirdCount++;
+            }
+            if (!$(this).hasClass("comment")) {
+                // If the row is not part of the "comment" class
+                // Assign pointsTemp to itself plus the value of the row, with any commas removed, and converted to a floating point
+                pointsTemp += parseFloat(($(this).text().replace(",", "")));
+                // Points is equal to pointsTemp, but uses this regex to add the commas back into the number
+                points = pointsTemp.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+            $('table:nth-child(4) > thead > tr > th:nth-child(' + i + ')').html(playerNameAndLink);
+            //insert the scores into the relevant medal tally
+            if (window.localStorage.getItem("spoiler") == "show") {
+                $('table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(' + i + ')').text(thirdCount);
+                $('table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(' + i + ')').text(secondCount);
+                $('table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(' + i + ')').text(firstCount);
+                $('table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(' + i + ')').text(points);
+            }
+            if (window.localStorage.getItem("spoiler") == "hide") {
+                $('table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(' + i + ')').text("Spoilers");
+                $('table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(' + i + ')').text("Spoilers");
+                $('table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(' + i + ')').text("Spoilers");
+                $('table:nth-child(4) > tbody > tr:nth-child(4) > td:nth-child(' + i + ')').text("Spoilers");
+            }
 
 
-            })
-            //reset the medal count for the next player
-            firstCount = 0, secondCount = 0, thirdCount = 0, points = 0, pointsTemp = 0;
-        }
+        })
+        //reset the medal count for the next player
+        firstCount = 0, secondCount = 0, thirdCount = 0, points = 0, pointsTemp = 0;
+    }
 
-    };
+}
