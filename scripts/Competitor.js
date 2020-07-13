a64dc473159f4370d47237eac9a9f3c6266c666c
bcranton <bcranton@users.noreply.github.com>
2020-07-12 17:04:08 -0700

    score update

--- scripts/Competitor.js
+++ scripts/Competitor.js
@@ -18,7 +18,7 @@ var competitors = [
 		"Handle": "dangheesling",
 		"Gold": 33,
 		"Silver": 30,
-		"Bronze": 85,
+		"Bronze": 86,
 		"Wins": [
 			"SEGA Classics",
 			"NES Classics"
@@ -29,7 +29,7 @@ var competitors = [
 		"Name": "Michael AL Fox",
 		"Handle": "MALF",
 		"Gold": 72,
-		"Silver": 50,
+		"Silver": 51,
 		"Bronze": 26,
 		"Wins": [
 			"Escape From Tarkov",
@@ -41,7 +41,7 @@ var competitors = [
 	{
 		"Name": "Ryan Letourneau",
 		"Handle": "Northernlion",
-		"Gold": 54,
+		"Gold": 55,
 		"Silver": 63,
 		"Bronze": 31,
 		"Wins": [
